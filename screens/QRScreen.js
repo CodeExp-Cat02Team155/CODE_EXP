c72abea33b8642783422cbed67d36dfb7313bee6
8kdesign <that8kguy@gmail.com>
2021-06-14 20:39:48 +0800

    Shorten app.js

--- screens/QRScreen.js
+++ screens/QRScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default function QRScreen({ navigation }) {
