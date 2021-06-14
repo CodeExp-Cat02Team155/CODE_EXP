acb74950b00f882551c22c3eb548e7e30225b27f
8kdesign <that8kguy@gmail.com>
2021-06-14 15:09:23 +0800

    Move favorites out

---
+++ screens/SearchScreen.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import {
+    View,
+    FlatList,
+} from 'react-native'
+
+export default function FavoritesScreen({navigation}) {
+
+    
+    return (<View style={{flex: 1}}>
+
+    </View>)
+}
